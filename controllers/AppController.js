03a7d5877afb2ba5e22cddf0555a3be9ecf3e7e7
Lapololucy <lapololucy@gmail.com>
2023-09-30 02:25:54 -0700

    updates

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,22 +1,19 @@
-import RedisClient from '../utils/redis';
-import DBClient from '../utils/db';
+/* eslint-disable import/no-named-as-default */
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
 
-class AppController {
+export default class AppController {
   static getStatus(req, res) {
-    const data = {
-      redis: RedisClient.isAlive(),
-      db: DBClient.isAlive(),
-    };
-    return res.status(200).send(data);
+    res.status(200).json({
+      redis: redisClient.isAlive(),
+      db: dbClient.isAlive(),
+    });
   }
 
-  static async getStats(req, res) {
-    const data = {
-      users: await DBClient.nbUsers(),
-      files: await DBClient.nbFiles(),
-    };
-    return res.status(200).send(data);
+  static getStats(req, res) {
+    Promise.all([dbClient.nbUsers(), dbClient.nbFiles()])
+      .then(([usersCount, filesCount]) => {
+        res.status(200).json({ users: usersCount, files: filesCount });
+      });
   }
 }
-
-module.exports = AppController;
--- server.js
+++ server.js
@@ -1,30 +1,12 @@
-
-import router from './routes/index';
-
-const express = require('express');
-
-const app = express();
-const port = process.env.PORT || 5000;
-
-router(app);
-
-app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
-});
-
 import express from 'express';
-import router from './routes/index';
+import startServer from './libs/boot';
+import injectRoutes from './routes';
+import injectMiddlewares from './libs/middlewares';
 
-const port = parseInt(process.env.PORT, 10) || 5000;
+const server = express();
 
-const app = express();
+injectMiddlewares(server);
+injectRoutes(server);
+startServer(server);
 
-app.use(express.json());
-app.use('/', router);
-
-app.listen(port, () => {
-  console.log(`server running on port ${port}`);
-});
-
-export default app;
->>>>>>> 10a1b0144f7b8a4de5ecd30e570c0434c4850ffc
+export default server;
